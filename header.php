d2ac750608142450e0c9fa7cf40f5866020a93ea
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2020-12-25 22:55:57 +0300

    Header Changes

--- header.php
+++ header.php
@@ -1,13 +1,15 @@
-<?
-include "variables.php";
-?>
-
+<? include "variables.php"; ?>
 <!DOCTYPE html>
 <html lang="tr">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="keywords" content="palet, Güray, Ahşap Palet, Euro Palet, İkinci el palet, Sıfır Palet">
+    <meta name="keywords" content="
+    <?php if (have_rows('meta', 'option')):
+          while (have_rows('meta', 'option')):
+             the_row();
+                 echo the_sub_field('meta_etiketi') .", ";
+    endwhile; endif;?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="author" content="Güray Palet">
     <title>
@@ -18,7 +20,7 @@ include "variables.php";
         } ?>
     </title>
 
-    <link rel="shortcut icon" href="<? echo $logo_url?>" type="image/png">
+    <link rel="shortcut icon" href="<? echo $logo_url ?>" type="image/png">
     <link rel="stylesheet" href="<? echo get_template_directory_uri() ?>/scss/css/bootstrap.min.css">
     <link rel="stylesheet" href="<? echo get_template_directory_uri() ?>/scss/css/animate.css">
     <link rel="stylesheet" href="<? echo get_template_directory_uri() ?>/scss/css/LineIcons.css">
@@ -34,7 +36,6 @@ include "variables.php";
 
 <body>
 
-<!-- Header Section Start -->
 <header id="home" class="hero-area">
     <div class="overlay">
         <span></span>
@@ -42,7 +43,7 @@ include "variables.php";
     </div>
     <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
         <div class="container">
-            <a href="/" class="navbar-brand"><img  style="max-height: 50px" src="<?echo $logo_url?>" alt="Güray Palet"></a>
+            <a href="/" class="navbar-brand"><img style="max-height: 50px" src="<? echo $logo_url ?>" alt="Güray Palet"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="lni-menu"></i>
@@ -50,7 +51,22 @@ include "variables.php";
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto w-100 justify-content-end">
                     <li class="nav-item">
-                        <a class="nav-link page-scroll" href="#home">Home</a>
+                        <a class="nav-link page-scroll" href="#home">Ana Sayfa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="#about">Hakkımızda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="#home">Ana Sayfa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="#about">Hakkımızda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="#home">Ana Sayfa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="#contact">İletişim</a>
                     </li>
 
                 </ul>
